c196c4ff8d8b7ed87964f1c8fc08013400ea6c2a
aiengineernlp <aiengineernlp@gmail.com>
2026-03-19 20:59:36 +0400

    A dataset exported from a database contains the following customer age values

--- assignments/listes-boucles-/Question 2.py
+++ assignments/listes-boucles-/Question 2.py
@@ -1,6 +1,10 @@
 ages = [25, 42, -1, 34, None, 52, 17, -5, 29, None, 46]
 # Iterates through the dataset and removes all invalid age values
+valid_ages = []
 for a in ages:
-    if type(a) != int:
-        ages.remove(a)
-        print(ages)
+    if type(a) is int and a >= 0:
+        if a not in valid_ages:
+            valid_ages.append(a)
+    else:
+        pass
+    print(valid_ages)
