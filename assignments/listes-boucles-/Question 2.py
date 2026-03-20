f1d1d975053035d9f6b66cae9a38e4b7f208179a
aiengineernlp <aiengineernlp@gmail.com>
2026-03-20 13:20:55 +0400

    A dataset exported from a database contains the following customer age values_finish

--- assignments/listes-boucles-/Question 2.py
+++ assignments/listes-boucles-/Question 2.py
@@ -1,28 +1,31 @@
 ages = [25, 42, -1, 34, None, 52, 17, -5, 29, None, 46]
 # Iterates through the dataset and removes all invalid age values
 valid_ages = []
+invalid_values = []
 for a in ages:
     if type(a) is int and a >= 0:
         if a not in valid_ages:
             valid_ages.append(a)
     else:
-        pass
-    '''Stores only valid ages in a new list.'''
+        invalid_values.append(a)
+# Prints how many invalid entries were removed during the cleaning process.
+print(invalid_values)
+print(f" Number of invalid entries:    {len(invalid_values)} values")
+# '''Stores only valid ages in a new list.'''
 print(valid_ages)
 print('\n')
-'''Calculates the average age of valid customers.'''
+# '''Calculates the average age of valid customers.'''
 average = 0
 for c in valid_ages:
     average =(sum(valid_ages)/len(valid_ages))
 print (f"\n the average is : {average}")
 
 print('\n')
-"""Displays the list of customers whose age is greater than 30."""
+# """Displays the list of customers whose age is greater than 30."""
 is_greater_list = []
 for a in valid_ages:
     if a>30:
         is_greater_list.append(a)
 print(is_greater_list)
-
 
 
