dfcee78f5588a7183699aff0661487fa71539144
aiengineernlp <aiengineernlp@gmail.com>
2026-04-17 14:10:52 +0400

    Assignment 3 LogicMojo_jour_11_12 1. Extract feature matrix as NumPy array 2. Compute: Mean Median Standard deviation Variance (for each feature) 3. Identify: Which feature has highest variability and why it matters in ML 4. Convert any one feature into shape (n,1) and explain why ML models expect this format

--- assignments/question_1.py
+++ assignments/question_1.py
@@ -6,10 +6,11 @@ import pandas as pd
 
 # print(data.head())
 # print(type(data))
-data = sns.load_dataset("iris")  # initial data iris
+data = sns.load_dataset("iris")  # initial data load from seaborn
 data_colums = data.columns
 
-def extract_feature(data: pd.DataFrame) -> np.ndarray:
+
+def extract_features(data: pd.DataFrame) -> np.ndarray:
     """
     This function extract features matrix as numpy array.
 
@@ -21,19 +22,61 @@ def extract_feature(data: pd.DataFrame) -> np.ndarray:
 
     Errors:
         - Raise DataNotFoundError
+        - ValueError
 
     """
     if data is not None:
+        #
         try:
             data_arr = np.array(data)
-            # mean = data.iloc[:,:-1].values
         except Exception as e:
-            raise ValueError(f"Need data {e}")
-    return data_arr
+            raise ValueError(f"Need data for to convert matrix to numpy array {e}")
+
+    # Computations
+    numeric_data = data.select_dtypes(include=[
+        np.number])  # je recupère uniquement les colonnes numeriques. ici "species" est eliminer directement du calcul
+    # numeric_data_cols = numeric_data.columns.tolist() # On récupère les noms des colonnes pour savoir ce qu'on manipule
+    # mean
+    mean_data_arr = np.mean(numeric_data, )
+    # Median
+    median_data_arr = np.median(numeric_data, )
+    # Standarddeviation
+    std_data_arr = np.std(
+        numeric_data, )  # np.std(numeric_data).sum()  🚩 do we need separatly for each feature or sum() them ?
+    # Variance
+    var_data_arr = np.var(numeric_data, )
+    # Convert one feature to shape (n,1)
+    data_arr_sepal_width = data["sepal_width"].values.reshape(-1, 1)
+    return data_arr, mean_data_arr, median_data_arr, std_data_arr, var_data_arr, data_arr_sepal_width
 
 
-# TEST
+data_arr, mean_data_arr, median_data_arr, std_data_arr, var_data_arr, data_arr_sepal_width = extract_features(data)
+print(f"{"█" * 70} TEST RESULTS {"█" * 55}")
+print(data)
+print(f" Extract feature matrix as NumPy array:")
+print(f'{'-' * 50}')
+print(f"{data_arr}\n")
+# ====================
+print(f" Computations:")
+print(f'{'-' * 50}')
+print(f" Mean:  {mean_data_arr:.2f}\n")
+# ====================
+print(f'{'-' * 50}')
+print(f" MeDian:  {median_data_arr:.2f}\n")
+# ====================
+print(f'{'-' * 50}')
+print(f" Standarddeviation:\n{std_data_arr}\n")
+# ====================
+print(f'{'-' * 50}')
+print(f" Variance:\n{var_data_arr}")
 
-r = extract_feature(data)
+print("\n========Curent type======")
 print(type(r))
-print(r.shape)
+print(type(data))
+print(f"{"█" * 70} ANALYSIS {"█" * 60}")
+print("Which feature has highest variability and why it matters in ML")
+print(
+    " ==>> Base on Standarddeviation and Variance,  sepal_length has the highest variability. It matters in ML because the help us to know if they are extremes values on our data. Also the model can thing that somme values are more importants than others just because of highs numbers ")
+print("Convert any one feature into shape (n,1) and explain why ML models expect this format")
+print(data_arr_sepal_width)
+
