ebc77d35439855d31be5a79e71a0d769ed3eda41
aiengineernlp <aiengineernlp@gmail.com>
2026-04-17 19:13:28 +0400

    import numpy as np import pandas as pd import seaborn as sns
    data = sns.load_dataset(titanic)
    
    def filter(data: pd.DataFrame)->pd.DataFrame:

---
+++ assignments/numpy_pandas_/question_5.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+data = sns.load_dataset("titanic")
+
+
+def filter(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function is for Filtering Data for Business Logic
+
+    Args:
+        - data: full titanic dataset imported with seaborn
+        -
+    Returns:
+        - DataFrame ready to take or make decision
+        -
+    Errors:
+        - Exceoption Error
+        - ValuesError
+    """
+    pass
+
+
