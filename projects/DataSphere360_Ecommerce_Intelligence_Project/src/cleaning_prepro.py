25858c7ae553f2233c177e61e58489253f94e057
aiengineernlp <aiengineernlp@gmail.com>
2026-04-25 03:14:23 +0400

    understanding_relation_between_tables special UPDATE TAKE A TOUR    R ✅

--- projects/DataSphere360_Ecommerce_Intelligence_Project/src/cleaning_prepro.py
+++ projects/DataSphere360_Ecommerce_Intelligence_Project/src/cleaning_prepro.py
@@ -3,3 +3,6 @@ from sqlalchemy import create_engine
 
 # -0- connection to the data base PostgreSql
 engine_erman_connexion_to__dataspere360 = create_engine('postgresql://postgres:postgres@localhost:5555/datasphere360_customer_ecommerce')
+
+
+
