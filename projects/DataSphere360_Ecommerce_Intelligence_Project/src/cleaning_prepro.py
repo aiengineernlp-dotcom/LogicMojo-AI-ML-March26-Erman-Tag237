cf54d6615624800080b5fea2e3b40ffb244a7d96
aiengineernlp <aiengineernlp@gmail.com>
2026-04-24 12:06:11 +0400

    update laad data beta

---
+++ projects/DataSphere360_Ecommerce_Intelligence_Project/src/cleaning_prepro.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# -0- connection to the data base PostgreSql
+engine_erman_connexion_to__dataspere360 = create_engine('postgresql://postgres:postgres@localhost:5555/datasphere360_customer_ecommerce')
