a140f4093ec69b16a7a23699b99675604e384845
aiengineernlp <aiengineernlp@gmail.com>
2026-05-21 13:12:26 +0400

    Initial exploration done with diagnostics on the function relation ship between tables

--- practice/DataSphere360_Ecommerce_Intelligence_Project_v2_beta/src/initial_exploration.py
+++ practice/DataSphere360_Ecommerce_Intelligence_Project_v2_beta/src/initial_exploration.py
@@ -75,8 +75,47 @@ def identify_primary_foreing_key(data_fetch_from_psql: dict) -> dict | str:
 r_c_identify_primary_foreing_key = identify_primary_foreing_key(r_c_fect_data_from_sql)
 print(r_c_identify_primary_foreing_key)
 
+import re
+def understand_relationbetweentable(data_from_sql: dict) -> dict | str:
+    print(f"{'▇' * 30} understand_relationbetween tables {'▇' * 70}")
+    """"""
+    pattern_keys = re.compile(r'.*(id|_id|code|zip_code|pk|fk)$|^id$|zip_code|^code$', re.IGNORECASE)
+    dict_all_keys = {}
+    for table_name, df in data_from_sql.items():
+        if not table_name:
+            raise ValueError("The table doest not exist ...")
+        else:
+            try:
+                potentials_cols = [col for col in df.columns if pattern_keys.match(col)]
+                for col in potentials_cols:
+                    is_nunique = df[col].nunique() == len(df)  # total de ligne unique doit etre egal a la taille du df
 
-     
-    
+                    key_relation = f'{table_name}.{col}' # juste pour l'affichage. on veut voir quelle colonne "cle" est liee a quelle table
+
+                    type_key = "PK" if is_nunique else "FK" # type de la clee
+
+                    dict_all_keys[key_relation] = type_key
+
+                    print(f"{table_name} is a {type_key} via the relation {key_relation}")
+
+            except Exception as e:
+                # print ("🚨✅🚩❌⚠️🔍⏳▇")
+                print("❌ Il ya une erreur ici c'est : ", e)
+
+    return dict_all_keys
+
+
+r_c_understand_relationbetweentable = understand_relationbetweentable(r_c_fect_data_from_sql)
+print(r_c_understand_relationbetweentable)
+
+"""
+> IN THE FONCTION "understand_relationbetweentable"  CODE  WE HAVE 3 FALSES POSITIVES
+1- orders is a PK via the relation orders.customer_id : SUPPOSE TO BE FK
+2- reviews is a FK via the relation reviews.review_id : SUPPOSE TO BE PK 
+3- order_item is a FK via the relation order_item.order_item_id : SUPPOSE TO BE PK 
+
+nevertheless It doest not affect the pipeline because this function is separeted from the other and he is independent.
+also his output is not yet used. When I will use it i will consider the false Positives observed 
+"""
     
     
