aa6e4d53fc65660c116b60e4633cd34b38989712
aiengineernlp <aiengineernlp@gmail.com>
2026-07-15 22:30:58 +0400

    eda Seller Analysis

--- projects/DataSphere360_Ecommerce_Intelligence_Project/data/eda_manu.py
+++ projects/DataSphere360_Ecommerce_Intelligence_Project/data/eda_manu.py
@@ -108,16 +108,14 @@ def f_eda_metier_peak_sales(df):
     # sum(payment_value)
     # lineplot
 
-
-
     # combiner année et mois pour avoir # ✅ "2017-01"
     df["year_month"] =df["order_purchase_timestamp_year"].astype(str) + "-" + df["order_purchase_timestamp_month"].astype(str) # ✅ donne "2017-01"
     """
-    on combine deux features avant de faire le groupby parceque ....
+    on combine deux features pour avoir plus de lisibiliter dans le rapport.
     """
 
     psm = df.groupby("year_month")["payment_value"].sum().sort_index()
-    fig = px.line(
+    fig = px.line( # vue que c'est une representation temporelle, on utilise px.line()
         x=psm.index,
         y=psm.values,
         title="Peak Sales",
@@ -126,6 +124,97 @@ def f_eda_metier_peak_sales(df):
     return fig
 
 
+
+
+def f_eda_metier_top_categories(df):
+    # groupby product_category_name
+    # compter les commandes uniques
+    # garder le top 10
+    # barplot
+    # retourner figure
+    top_categories = df.groupby("product_category_name")['order_id'].nunique().sort_values(ascending=False).head(10)
+
+    fig = px.bar(
+        x=top_categories.index,
+        y=top_categories.values,
+        title="Top Categories",
+    )
+
+    return fig
+
+
+def f_eda_metier_revenue_by_category(df):
+    # groupby product_category_name
+    # sum(payment_value)
+    # top 10
+    # barplot
+    # retourner figure
+
+    revenue_by_category = df.groupby("product_category_name")["payment_value"].sum().sort_values(ascending=False).head(10)
+
+    fig = px.bar( x=revenue_by_category.index,y=revenue_by_category.values,title="Revenue by Category")
+
+    return fig
+
+
+
+
+
+def f_eda_metier_product_demand(df):
+    # compter combien de fois chaque produit est commandé
+    product_demand = df["product_id"].value_counts()
+    # créer un histogram
+    fig = px.histogram(
+        x=product_demand.values,
+        title="Product Demand Distribution",
+        labels={"x": "Nombre de commandes par produit"}
+    )
+    # retourner figure
+    return fig
+
+
+def f_eda_metier_top_sellers(df):
+    # groupby seller_id
+    # compter les commandes uniques
+    # top 10
+    # barplot
+    # retourner figure
+
+    top_sellers = df.groupby("seller_id")["order_id"].nunique().sort_values(ascending=False).head(10)
+
+    fig =px.bar(
+        x=top_sellers.index,
+        y=top_sellers.values,
+        title="Top sellers",
+    )
+
+    return fig
+
+
+def f_eda_metier_seller_revenue(df):
+    # groupby seller_id
+    # sum(payment_value)
+    # top 10
+    # barplot
+    # retourner figure
+    seller_revenue = df.groupby("seller_id")["payment_value"].sum().sort_values(ascending=False).head(10)
+    '''
+    groupby sur ("seller_id") parce que c'est dans seller_id que on va voir le montant vendu
+    sum() sur ["payment_value"] pour sommer le montant du seller
+    '''
+
+    fig = px.bar( x=seller_revenue.index,y=seller_revenue.values,title="Seller Revenue") # bar parceque on a plusieurs payment_value mais on a juste choisi d'afficher 10
+    return fig
+
+
+def f_eda_metier_seller_distribution(df):
+    # compter le nombre de vendeurs par état
+    # barplot
+    # retourner figure
+    seller_distribution = df.groupby("seller_state")["seller_id"].nunique().sort_values(ascending=False).head(10)
+
+    fig = px.bar( x=seller_distribution.index,y=seller_distribution.values,title="Seller Distribution")
+    return fig
 
 
 '''
